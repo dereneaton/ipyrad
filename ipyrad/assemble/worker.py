b0732eaf6841a6a323fb0fc86c6d35453e878959
deren <daeaton.chicago@gmail.com>
2015-10-08 22:12:55 -0400

    moved ObjDict Class here

--- ipyrad/assemble/worker.py
+++ ipyrad/assemble/worker.py
@@ -33,3 +33,22 @@ class Worker(multiprocessing.Process):
             res = self.func(*job)
             # store the result
             self.result_queue.put(res)
+
+
+class ObjDict(dict):
+    """ object dictionary allows calling dictionaries in a more 
+    pretty and Python fashion for storing Assembly data """
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
