e8365046e2780623e8494c6b6f4625fe97a2b866
deren <daeaton.chicago@gmail.com>
2015-11-05 15:12:40 -0500

    sphinx ughhh

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["sphinx.ext.autodoc",
-              "sphinxcontrib.napolean"]
+              "sphinx.ext.napolean"]
 
 # Napoleon settings
 napoleon_google_docstring = True
