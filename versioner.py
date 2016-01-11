c8d2b98de7eb2166da9ae6503cf7f05498e37607
isaacovercast <isaac.overcast@gmail.com>
2016-01-11 17:02:03 -0500

    Moved git tag script to versioner.py and updated so it hax the default log level too.

--- versioner.py
+++ versioner.py
@@ -17,6 +17,8 @@ for line in fileinput.input(initfile, inplace=1):
         if "__version__" in line:
             line = "__version__ = \""+version_git+"\""
         print(line.strip("\n"))
+        elif: "__loglevel__" in line:
+            line = "__loglevel__ = \"ERROR\""
 try:
     subprocess.call(["git", "add", initfile])
     subprocess.call(["git", "commit", "-m \"Updating ipyrad/__init__.py to "+\
