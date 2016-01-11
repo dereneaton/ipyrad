76bb7775166aedb841b1fc12f20d510f42ad0977
isaacovercast <isaac.overcast@gmail.com>
2016-01-10 21:29:17 -0500

    "Updating ipyrad/__init__.py to version - 0.1.9

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python2.7
 
 from setuptools import setup, find_packages
-import subprocess
-import fileinput
 import glob
-import os
 import re
 
 requirements = [
@@ -30,37 +27,14 @@ requirements = [
 # Also, when git is not available (PyPi package), use stored version.py.
 initfile = "ipyrad/__init__.py"
 
-cur_version = version_git = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+cur_version = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
                     open(initfile, "r").read(),
                     re.M).group(1)
-
-# If git is available and working pull version from github, else read from ipyrad/__init__.py
-try:
-    version_git = subprocess.check_output(["git", "describe"]).rstrip().split("-")[0]
-except:
-    version_git = cur_version
-
-if( not cur_version == version_git ):
-    # Write version to ipyrad/__init__.py
-    for line in fileinput.input(initfile, inplace=1):
-            if "__version__" in line:
-                line = "__version__ = \""+version_git+"\""    
-            print(line.strip("\n"))
-    subprocess.call(["git", "add", initfile])
-    subprocess.call(["git", "commit", "-m \"Updating ipyrad/__init__.py to"+\
-                        "version - {}".format(version_git)])
-    subprocess.call(["git", "push"])
 
 setup(
     name="ipyrad",
 
-    version = version_git,
-#    version=re.search(
-#       r"^__version__ = ['\"]([^'\"]*)['\"]",
-#           open(
-#               "ipyrad/__init__.py",
-#               "r").read(),
-#       re.M).group(1),
+    version = cur_version,
 
     url="https://github.com/dereneaton/ipyrad",
 
