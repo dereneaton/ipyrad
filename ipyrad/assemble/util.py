4b1a97b5145d080bfb405d6dcf9baa5594a4e855
deren <daeaton.chicago@gmail.com>
2016-01-11 19:31:49 -0500

    moved ObjDict to util

--- ipyrad/assemble/util.py
+++ ipyrad/assemble/util.py
@@ -24,6 +24,25 @@ AMBIGS = {"R":("G", "A"),
           "M":("C", "A")}
 
 
+class ObjDict(dict):
+    """ object dictionary allows calling dictionaries in a more 
+    pretty and Python fashion for storing Assembly data """
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+
 def ambigcutters(seq):
     """ Returns both resolutions of a cut site that has an ambiguous base in 
     it, else the single cut site """
@@ -76,16 +95,15 @@ def comp(seq):
 
 
 def getsplits(filename):
-    """ Calculate optimum splitting based on file size. 
-    Does not unzip files, assumes average rate of compression. 
-    This is a fast alternative to counting lines which takes 
-    too long on huge files.
+    """ Calculate optimum splitting based on file size. Does not unzip files, 
+    assumes average rate of compression. This is a fast alternative to counting 
+    lines which takes too long on huge files.
     """
     filesize = os.stat(filename).st_size
     if filesize < 10000000:
-        optim = 400000
+        optim = 200000
     elif filesize < 4000000000:
-        optim = 1000000
+        optim = 500000
     elif filesize < 8000000000:
         optim = 4000000
     else:
@@ -287,13 +305,8 @@ def zcat_make_temps(args):
     """
 
     ## split args
-    data, raws, num, optim = args
+    data, raws, num, tmpdir, optim = args
     LOGGER.debug("zcat splittin' %s", os.path.split(raws[0])[-1])
-
-    ## make tmpdir to hold chunks
-    tmpdir = os.path.join(data.dirs.working, "tmpchunks")
-    if not os.path.exists(tmpdir):
-        os.mkdir(tmpdir)
 
     ## get optimum lines per file
     if not optim:
