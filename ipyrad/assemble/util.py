89e0ad9c54bd4eaf554bb965782c19dfee067a39
deren <daeaton.chicago@gmail.com>
2016-01-23 01:38:02 -0500

    moved fullcomp here

--- ipyrad/assemble/util.py
+++ ipyrad/assemble/util.py
@@ -121,6 +121,39 @@ def comp(seq):
 
 
 
+def fullcomp(seq):
+    """ returns complement of sequence including ambiguity characters,
+    and saves lower case info for multiple hetero sequences"""
+    ## this is surely not the most efficient...
+    seq = seq.replace("A", 'u')\
+             .replace('T', 'v')\
+             .replace('C', 'p')\
+             .replace('G', 'z')\
+             .replace('u', 'T')\
+             .replace('v', 'A')\
+             .replace('p', 'G')\
+             .replace('z', 'C')
+
+    ## No complement for S & W b/c complements are S & W, respectively
+    seq = seq.replace('R', 'u')\
+             .replace('K', 'v')\
+             .replace('Y', 'b')\
+             .replace('M', 'o')\
+             .replace('u', 'Y')\
+             .replace('v', 'M')\
+             .replace('b', 'R')\
+             .replace('o', 'K')
+
+    seq = seq.replace('r', 'u')\
+             .replace('k', 'v')\
+             .replace('y', 'b')\
+             .replace('m', 'o')\
+             .replace('u', 'y')\
+             .replace('v', 'm')\
+             .replace('b', 'r')\
+             .replace('o', 'k')
+    return seq
+
 
 def merge_pairs(data, sample): #, unmerged_files):
     """ 
