f4cb48f17a1236460ec9174a0e1d6a66df1987e4
isaacovercast <isaac.overcast@gmail.com>
2016-02-19 17:28:57 -0500

    Added a function to return the parameter name given an integer index

--- ipyrad/core/paramsinfo.py
+++ ipyrad/core/paramsinfo.py
@@ -436,6 +436,22 @@ pinfo = OrderedDict([
 
 ])
 
+def paramname(param=""):
+    """ Get the param name from the dict index value.
+    """
+
+    try: 
+        name = pinfo[str(param)][0].strip().split(" ")[1]
+    except (KeyError, ValueError) as err:
+        ## TODO: paramsinfo get description by param string not working.
+        ## It would be cool to have an assembly object bcz then you could
+        ## just do this:
+        ##
+        ## print(pinfo[data.paramsinfo.keys().index(param)])
+        print("\tKey name/number not recognized - ".format(param), err)
+        raise
+
+    return name
 
 def paraminfo(param="", short=False):
     """ Returns detailed information for the numbered parameter. 
