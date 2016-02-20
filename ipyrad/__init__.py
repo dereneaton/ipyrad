3b8116f983f7110f450988df08029ab00048028e
deren <daeaton.chicago@gmail.com>
2016-02-20 16:48:44 -0500

    "Updating ipyrad/__init__.py to version - 0.1.54

--- ipyrad/__init__.py
+++ ipyrad/__init__.py
@@ -4,10 +4,10 @@
 
 ## define state vars
 __interactive__ = 1      ## CLI __main__ changes to 0
-__version__ = "0.1.53"
+__version__ = "0.1.54"
 
 ## Possible values for __loglevel__: "DEBUG"  "INFO"  "WARN"  "ERROR"
-__loglevel__ = "DEBUG"
+__loglevel__ = "ERROR"
 __debugfile__ = "/tmp/ipyrad_debug.txt"
 
 ## main ip.functions
