6b1ca08b2988e02af410a9de732ec605a10ff1e9
deren <daeaton.chicago@gmail.com>
2016-02-23 03:06:06 -0500

    fix for when no permissions to use /tmp for logger

--- ipyrad/__init__.py
+++ ipyrad/__init__.py
@@ -42,7 +42,7 @@ try:
                 clear.write("file reset")
 
 ## in case system doesn't let you use /tmp            
-except (OSError, IOError):
+except (OSError, IOError, ValueError):
     __debugfile__ = "./ipyrad_debug.txt"  ##_os.devnull
     _, __loglevel__ = "null", "ERROR"  ## hack for versioner
 
@@ -64,7 +64,7 @@ _lconfig.dictConfig({
         __name__: {
             'level':__loglevel__,
             'class':'logging.FileHandler',
-            'filename':'/tmp/ipyrad_debug.txt',
+            'filename':__debugfile__,
             'formatter':"standard",
             'mode':'a+'
         }
