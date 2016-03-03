b39f38bcb5221ee7975fbe9c07d8c77e8358ac10
deren <daeaton.chicago@gmail.com>
2016-03-03 11:25:52 -0500

    svd bootstrap beginnings. Fix to step6 local storage

--- ipyrad/assemble/cluster_across.py
+++ ipyrad/assemble/cluster_across.py
@@ -351,7 +351,7 @@ def singlecat(data, sample, nloci, indels):
     Orders catg data for each sample into the final locus order. This allows
     all of the individual catgs to simply be combined later. They are also in 
     the same order as the indels array, so indels are inserted from the indel
-    array that in passed in. 
+    array that is passed in. 
     """
     LOGGER.debug("singlecat: %s", sample.name)
 
@@ -398,6 +398,12 @@ def singlecat(data, sample, nloci, indels):
     for iloc, seed in enumerate(udic.groups.iterkeys()):
         ipdf = udic.get_group(seed)
         ask = ipdf.where(ipdf[3] == sample.name).dropna()
+
+        ## write to disk after 10000 writes
+        if not iloc % 10000:
+            icatg[start:iloc] = local[:]
+            start = iloc
+
         if ask.shape[0] == 1: 
             ## if multiple hits of a sample to a locus then it is not added
             ## to the locus, and instead the locus is masked for exclusion
@@ -408,10 +414,6 @@ def singlecat(data, sample, nloci, indels):
             ## store that this cluster failed b/c it had duplicate samples. 
             dupfilter[iloc] = True
 
-        ## write to disk after 10000 writes
-        if not iloc % 10000:
-            icatg[start:iloc] = local[:]
-            start = iloc
     ## write the leftover 
     icatg[start:] = local[:icatg[start:].shape[0]]
 
