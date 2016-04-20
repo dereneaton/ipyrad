d3d8697d31e6da8bed8af8f37185c391e96e6436
dereneaton <deren.eaton@yale.edu>
2016-04-20 15:22:28 -0400

    massive rewrite, progress bars, apply, no more zcats

--- ipyrad/assemble/rawedit.py
+++ ipyrad/assemble/rawedit.py
@@ -13,6 +13,8 @@ import os
 import glob
 import gzip
 import math
+import time
+import datetime
 import itertools
 import numpy as np
 import shutil
@@ -146,7 +148,9 @@ def afilter(data, sample, bases, cuts1, cuts2, read):
 
 
 def adapterfilter(args):
-    """ filters for adapters """
+    """ 
+    Filters one read for Illumina adapters and appends to a list for writing
+    """
 
     ## parse args
     data, sample, read1, read2, cuts1, cuts2, write1, write2, point = args
@@ -219,7 +223,7 @@ def adapterfilter(args):
                                #read2[2].tostring(),
                                read2[3].tostring()])
             write2.append(sseq2)
-    return write1, write2, read1, read2, point, kept
+    return write1, write2, kept
 
 
 
@@ -228,43 +232,33 @@ def rawedit(args):
     Applies two filters: Nfilter and adapterfilter.
     """
     ## parse args
-    data, sample, tmptuple, nreplace, point = args
+    data, sample, num, nreplace, optim = args
+    point = num*optim
 
     ## get cut sites 
     cuts1, cuts2 = [ambigcutters(i) for i in \
                     data.paramsdict["restriction_overhang"]]
-    #LOGGER.debug("cutsites %s %s", cuts1, cuts2)
-    #LOGGER.debug([i for i in data.paramsdict["restriction_overhang"]])
+    #LOGGER.info("cutsites %s %s", cuts1, cuts2)
+    #LOGGER.info([i for i in data.paramsdict["restriction_overhang"]])
 
-    ## the read1 demultiplexed reads file
-    if tmptuple[0].endswith(".gz"):
-        fr1 = gzip.open(os.path.realpath(tmptuple[0]), 'rb')
-    else:
-        fr1 = open(os.path.realpath(tmptuple[0]), 'rb')
+    ## make quarts iterator to sample (r1,r2) or (r1, 1)
+    tmpdir = os.path.join(data.dirs.project, data.name+"-tmpchunks")
+    read1 = os.path.join(tmpdir, "tmp_{}_{}_R1.fastq".format(sample.name, num))
+    read2 = os.path.join(tmpdir, "tmp_{}_{}_R2.fastq".format(sample.name, num))
 
-    ## the read2 demultiplexed reads file, if paired
-    if "pair" in data.paramsdict["datatype"]:
-        if tmptuple[1].endswith(".gz"):
-            fr2 = gzip.open(os.path.realpath(tmptuple[1]), 'rb')
-        else:
-            fr2 = open(os.path.realpath(tmptuple[1]), 'rb')
-
-    ## create iterators to sample 4 lines at a time 
+    fr1 = open(read1, 'rb')
     quart1 = itertools.izip(*[iter(fr1)]*4)
     if "pair" in data.paramsdict["datatype"]:
-        ## pair second read files, quarts samples both
+        fr2 = open(read2, 'rb')
         quart2 = itertools.izip(*[iter(fr2)]*4)
         quarts = itertools.izip(quart1, quart2)
     else:
-        ## read in single end read file, quarts samples 1 for other
         quarts = itertools.izip(quart1, iter(int, 1))
 
     ## counters 
     counts = {}
-    counts["orig"] = 0
-    counts["quality"] = 0
-    counts["adapter"] = 0    
-    counts["keep"] = 0    
+    for key in ["orig", "quality", "adapter", "keep"]:
+        counts[key] = 0
 
     ## in memory storage to write to file
     write1 = []
@@ -294,10 +288,9 @@ def rawedit(args):
                 read1, read2 = modify_cuts(data, read1, read2)
 
             ## filter for adapters, and trim to size if pairgbs
-            args = [data, sample, read1, read2, cuts1, cuts2, 
-                    write1, write2, point]
-            write1, write2, read1, read2, point, kept = adapterfilter(args)
-            #LOGGER.debug("down HERE")
+            args = [data, sample, read1, read2, 
+                    cuts1, cuts2, write1, write2, point]
+            write1, write2, kept = adapterfilter(args)
 
             ## counters
             if kept:
@@ -400,130 +393,151 @@ def nfilter(data, read1, read2, nreplace=True):
 
 
 
-def prechecks(data):
-    """ checks before starting analysis """
-    ## link output directories 
-    data.dirs.edits = os.path.join(os.path.realpath(
-                                   data.paramsdict["project_dir"]), 
-                                   data.name+"_edits")
-    ## create output directory if doesn't exist
-    if not os.path.exists(data.dirs.edits):
-        os.makedirs(data.dirs.edits)
-
-
-
 def roundup(num):
     """ round to nearest hundred """
     return int(math.ceil(num / 100.0)) * 100
 
 
 
-def run_sample(data, sample, nreplace, preview, ipyclient):
-    """ 
-    Splits fastq file into smaller chunks and distributes them across
-    multiple processors, and runs the rawedit func on them .
-    """
-    ## before starting
-    prechecks(data)
-    num = 0
+def pre_run(data):
+    """ setup and optim """
+    ## link output directories 
+    data.dirs.edits = os.path.join(os.path.realpath(
+                                   data.paramsdict["project_dir"]), 
+                                   data.name+"_edits")
 
-    ## set optim size, can only be optimized if reads_raw 
-    optim = 10000
-    if sample.stats.reads_raw:
-        ### split reads among N processors
-        inputreads = sample.stats.reads_raw
-        ncpus = len(ipyclient)
-        ## The goal is to get the optimum number of lines per chunk so
-        ## as to split into 2 * ncpus chunks. Devide number of input reads
-        ## by 2*ncpus, then multipy by 4 to get the number of lines
-        ## Cast to an int, since numreads should be a whole number and
-        ## zcat_make_temps doesn't like the '100.0' float part.
-        ## Here optim is measured in nreads.
-        optim = (inputreads // (ncpus * 2)) + (inputreads % (ncpus * 2))
-        ## multiply by 4 to ensure fastq quartet sampling
-        ## The unit for optim is now nlines
-        optim *= 4
-        LOGGER.info("optim=%s", optim)
+    ## create output directory if doesn't exist
+    if not os.path.exists(data.dirs.edits):
+        os.makedirs(data.dirs.edits)
 
-    ## truncate fastq if preview-mode. For step 2 preview mode we slice off
-    ## 4 optim size chunks so there is one chunk for each of the default
-    ## 4 ipyclients to work on.
-    if preview:
-        nlines = data._hackersonly["preview_step2"]
-        optim = nlines // len(ipyclient)
-        LOGGER.warn("""\
-    Running preview mode: subselecting {} reads from {}
-    """.format(nlines, sample.files.fastqs))
-        sample_fastq = preview_truncate_fq(data, sample.files.fastqs, nlines)
-    else:
-        sample_fastq = sample.files.fastqs
+    # ## set optim size, can only be optimized if reads_raw 
+    # optim = 10000
+    # ncpus = detect_cpus()
+    # inputreads = int(data.stats.reads_raw.max())
+    # if inputreads:
+    #     ## Here optim is measured in reads.
+    #     optim = (inputreads // (ncpus)) + (inputreads % (ncpus)) 
+    #     ## multiply by 4 to get the number of lines
+    #     optim = int(optim*4)
+    #     LOGGER.info("optim=%s lines", optim)
 
-    LOGGER.debug("sample_fastq back here is %s", sample_fastq)
-    ## break up the file into smaller tmp files for each processor
+    # ## truncate fastq if preview-mode. For step 2 preview mode we slice off
+    # ## 4 optim size chunks so there is one chunk for each of the default
+    # ## 4 ipyclients to work on.
+    # if preview:
+    #     nlines = data._hackersonly["preview_step2"]
+    #     optim = int(nlines // ncpus())
+    #     LOGGER.warn("""\
+    # Running preview mode: subselecting only the first {} reads"""\
+    # .format(nlines))
+
+    ## tmpdir for slices
     tmpdir = os.path.join(data.dirs.project, data.name+"-tmpchunks")
     if not os.path.exists(tmpdir):
         os.mkdir(tmpdir)
-    chunkslist = []
-    for fastqtuple in sample_fastq:
-        args = [data, fastqtuple, num, tmpdir, optim]
-        _, achunk = zcat_make_temps(args)
-        chunkslist += achunk
 
-    LOGGER.info("Executing %s file, in %s chunks, across %s cpus", \
-                 len(sample_fastq), len(chunkslist), len(ipyclient))
-
-    ## send chunks across processors, will delete if fail
-    try:
-        submitted_args = []
-        for tmptuple in chunkslist:
-            ## point is used to increment names across processors
-            point = num*(optim // 4)  
-            submitted_args.append([data, sample, tmptuple, nreplace, point])
-            num += 1
-
-        ## create view to auto spread chunks across engines
-        lbview = ipyclient.load_balanced_view()
-        results = lbview.map_async(rawedit, submitted_args)
-        results.get()
-
-    except KeyboardInterrupt as inst:
-        ## Cleans up tmp files in the edits directory if rawedit passes,
-        ## but gets killed before sample_cleanup
-        tmpedit1 = glob.glob(os.path.join(data.dirs.edits,
-                    "tmp1_"+sample.name+"_*.fq"))
-        tmpedit2 = glob.glob(os.path.join(data.dirs.edits,
-                    "tmp2_"+sample.name+"_*.fq"))
-        try:
-            for i in tmpedit1:
-                os.remove(i)
-            for i in tmpedit2:
-                os.remove(i)
-        except:
-            pass
-        raise inst
-
-    finally:
-        ## if process failed at any point delete temp files
-        tmpdirs = glob.glob(os.path.join(data.dirs.project, 
-                                         data.name+"-tmpchunks"))
-        if tmpdirs:
-            for tmpdir in tmpdirs:
-                try:
-                    shutil.rmtree(tmpdir)   
-                except OSError:
-                    ## block again nfs error
-                    LOGGER.warn("Failed to remove tmpdir {}".format(tmpdir))
-
-        if preview:
-            ## cleans up tmp files generated by preview
-            for tmpfile in sample_fastq[0]:
-                os.remove(tmpfile)
-
-    return num, results
+    #return optim
 
 
 
-def sample_cleanup(data, sample, submitted, results):
+def send_slices(args):
+    """ 
+    Splits fastq file into smaller chunks.
+    """
+    ## parse args
+    data, sample, optim = args
+
+    ### location for slices
+    tmpdir = os.path.join(data.dirs.project, data.name+"-tmpchunks")
+
+    ## iterate over samples
+    num = 0
+    ## open file handles
+    tups = sample.files.fastqs[0]
+
+    if tups[0].endswith(".gz"):
+        io1 = gzip.open(tups[0])
+        rawr1 = iter(io1)
+        if tups[1]:
+            io2 = gzip.open(tups[1])
+            rawr2 = iter(io2)
+    else:
+        io1 = open(tups[0])
+        rawr1 = iter(io1)
+        if tups[1]:
+            io2 = open(tups[1])
+            rawr2 = iter(io2)
+
+    ## creating 10 slices per sample
+    for num in range(10):
+        dat1 = "".join(itertools.islice(rawr1, int(optim*4)))
+        if tups[1]:
+            dat2 = "".join(itertools.islice(rawr2, int(optim*4)))                
+
+        ## send slices
+        if dat1:
+            ## get outhandle
+            out1 = os.path.join(tmpdir, "tmp_{}_{}_R1.fastq"\
+                            .format(sample.name, num))
+            ## write slice
+            with open(out1, 'w') as tmpout:
+                tmpout.write(dat1)
+                
+            ## if paired-end
+            if tups[1]:
+                ## get outhandle
+                out2 = os.path.join(tmpdir, "tmp_{}_{}_R2.fastq"\
+                                .format(sample.name, num))
+                ## write slice
+                with open(out2, 'w') as tmpout:
+                    tmpout.write(dat2)
+                ## advance counter
+                num += 1
+
+    io1.close()
+    if tups[1]:
+        io2.close()
+
+
+
+# def fake():
+#     ## Cleans up tmp files in the edits directory if rawedit passes,
+#         ## but gets killed before sample_cleanup
+#         tmpedit1 = glob.glob(os.path.join(data.dirs.edits,
+#                     "tmp1_"+sample.name+"_*.fq"))
+#         tmpedit2 = glob.glob(os.path.join(data.dirs.edits,
+#                     "tmp2_"+sample.name+"_*.fq"))
+#         try:
+#             for i in tmpedit1:
+#                 os.remove(i)
+#             for i in tmpedit2:
+#                 os.remove(i)
+#         except:
+#             pass
+#         raise inst
+
+#     finally:
+#         ## if process failed at any point delete temp files
+#         tmpdirs = glob.glob(os.path.join(data.dirs.project, 
+#                                          data.name+"-tmpchunks"))
+#         if tmpdirs:
+#             for tmpdir in tmpdirs:
+#                 try:
+#                     shutil.rmtree(tmpdir)   
+#                 except OSError:
+#                     ## block again nfs error
+#                     LOGGER.warn("Failed to remove tmpdir {}".format(tmpdir))
+
+#         if preview:
+#             ## cleans up tmp files generated by preview
+#             for tmpfile in sample_fastq[0]:
+#                 os.remove(tmpfile)
+
+#     return num, results
+
+
+
+def sample_cleanup(data, sample, results):
     """ cleaning up for one sample """
 
     ## rejoin chunks
@@ -566,7 +580,7 @@ def sample_cleanup(data, sample, submitted, results):
                "keep": 0}
 
     ## merge finished edits
-    for i in range(submitted):
+    for i in range(10):
         counts = results[i]
         fcounts["orig"] += counts["orig"]
         fcounts["quality"] += counts["quality"]
@@ -648,31 +662,86 @@ def run(data, samples, nreplace, force, preview, ipyclient):
                 subsamples.append(sample)
 
     ## print to screen if preview method
+    optims = {}
+    for sample in subsamples:
+        if preview:
+            tots = data._hackersonly["preview_step2"]
+            optims[sample.name] = (tots // 10) + (tots % 10)
+        else:
+            tots = int(sample.stats.reads_raw)
+            optims[sample.name] = (tots // 10) + (tots % 10)
+
     if preview:
-        nlines = data._hackersonly["preview_step2"]
         if data._headers:
             print("""\
     Running preview mode: subselecting maximum of {} reads per sample\
-    """.format(nlines))
-
+    """.format(data._hackersonly["preview_step2"]))
 
     ## RUN THE SAMPLES
+    pre_run(data)
+
+    ## slicer slices off chunks to tmpdir
+    lbview = ipyclient.load_balanced_view()
+
+    ## start progress
+    start = time.time()
+    elapsed = datetime.timedelta(seconds=int(0))
+    progressbar(len(subsamples), 0, 
+               " processing jobs  | {}".format(elapsed))            
+
+    ## send jobs to slicer
+    sent = {} # {i.name:[] for i in subsamples}
+    for sample in subsamples:
+        optim = optims[sample.name]
+        sent[sample.name] = lbview.apply(send_slices, [data, sample, optim])
+
+    ## send slices to queue 
+    ## we know there will be 10 slices per sample.
+    sliced = {i.name:[] for i in subsamples}    
+    for sname in sent:
+        with lbview.temp_flags(after=sent[sname]):
+            for i in range(10):
+                args = [data, data.samples[sname], i, nreplace, optim]
+                sliced[sname].append(lbview.apply(rawedit, args))
+
+    ## print progress
     try:
-        for njob, sample in enumerate(subsamples):
-            sub, res = run_sample(data, sample, nreplace, preview, ipyclient)
-            sample_cleanup(data, sample, sub, res)
-            progressbar(len(subsamples), njob)
-        progressbar(len(subsamples), len(subsamples))
-        print("")
-    ## ensure the assembly is cleaned up even if stopped
+        while 1:
+            asyncs = list(itertools.chain(*sliced.values()))+sent.values()
+            tots = len(asyncs)
+            done = sum([i.ready() for i in asyncs])
+            elapsed = datetime.timedelta(seconds=int(time.time()-start))
+            if tots != done:
+                progressbar(tots, done,
+                           " processing reads  | {}".format(elapsed))            
+                time.sleep(1)
+            else:
+                progressbar(tots, done,
+                           " processing reads  | {}".format(elapsed))
+                if data._headers:
+                    print("")
+                break
+
+    except (KeyboardInterrupt, SystemExit):
+        print('\n  Interrupted! Cleaning up... ')
+        raise
+
+    ## enforced cleanup
     finally:
+        ## if all jobs were successful in a sample then cleanup
+        for sample in subsamples:
+            if all([i.successful() for i in sliced[sample.name]]):
+                results = [i.get() for i in sliced[sample.name]]
+                sample_cleanup(data, sample, results)
+
+        ## print errors if they occurred
+        for sample in sliced:
+            for async in sliced[sample]:
+                if not async.successful():
+                    print(async.metadata.error)
+
+        ## do final stats and cleanup
         assembly_cleanup(data)
-
-
-    ## Alternative execution. One proc keeps slicing data while others run.
-
-
-
 
 
 
