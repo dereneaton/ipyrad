57646c90737087974427f9f4a72ae65868540d65
isaacovercast <labuser@Trashquatch.local>
2016-10-22 19:34:50 -0400

    Fixed a bug w/ testing for bgzip reference sequence. Also add code to fix how PE ref is handled to address #148.

--- ipyrad/assemble/refmap.py
+++ ipyrad/assemble/refmap.py
@@ -81,11 +81,10 @@ def index_reference_sequence(data, force=False):
     ## error handling
     if proc1.returncode:
         raise IPyradWarningExit(error1)
-    if proc2.returncode:
-        if "please use bgzip" in error2:
-            raise IPyradWarningExit(NO_ZIP_BINS.format(refseq_file))
-        else:
-            raise IPyradWarningExit(error2)
+    if "please use bgzip" in error2:
+        raise IPyradWarningExit(NO_ZIP_BINS.format(refseq_file))
+    else:
+        raise IPyradWarningExit(error2)
 
     ## print finished message
     if data._headers:
@@ -564,6 +563,31 @@ def bedtools_merge(data, sample):
     return result
 
 
+def trim_reference_sequence(fasta):
+    """
+    If doing PE and R1/R2 don't overlap then the reference sequence
+    will be quite long and will cause indel hell during the 
+    alignment stage. Here trim the reference sequence to the length
+    of the merged reads. Input is a list of alternating locus labels
+    and sequence data. The first locus label is the reference
+    sequence label and the first seq is the reference seq. Returns
+    the same list except with the reference sequence trimmed to
+    the length of the rad tags
+    """
+    LOGGER.debug("pre - {}".format(fasta[0]))
+
+    ## If the reads are merged then the reference sequence should be the
+    ## same length as the merged pair. If unmerged then we have to fix it.
+    if "nnnn" in fasta[1]:
+        r1_len = len(fasta[1].split("\n")[1].split("nnnn")[0])
+        r2_len = len(fasta[1].split("\n")[1].split("nnnn")[1])
+        new_seq = fasta[0].split("\n")[1][:r1_len]+("nnnn")\
+                    + revcomp(fasta[0].split("\n")[1][-r2_len:])
+        fasta[0] = fasta[0].split("\n")[0]+"\n"+new_seq
+
+    LOGGER.debug("post - {}".format(fasta[0]))
+    return fasta
+
 
 def bam_region_to_fasta(data, sample, chrom, region_start, region_end):
     """ 
@@ -619,7 +643,7 @@ def bam_region_to_fasta(data, sample, chrom, region_start, region_end):
         ## TODO: Can we include the reference sequence in the PE clust.gz?
         ## if it's longer than the merged pairs it makes hella indels
         ## Drop the reference sequence for now...
-        fasta = []
+        #fasta = []
 
         ## Create temporary files for R1, R2 and merged, which we will pass to
         ## the function merge_pairs() which calls vsearch to test merging.
@@ -664,6 +688,12 @@ def bam_region_to_fasta(data, sample, chrom, region_start, region_end):
                         e=bits[1].strip())
                     #,e=bits[9])
                     fasta.append(fullfast)
+                ## If doing PE and R1/R2 don't overlap then the reference sequence
+                ## will be quite long and will cause indel hell during the 
+                ## alignment stage. Here trim the reference sequence to the length
+                ## of the merged reads
+                fasta = trim_reference_sequence(fasta)
+
             ## clean up
             os.remove(merged)
             os.remove(read1)
