7b02ba8892d24e1ce9e5b806b9bfaa16c0da57ba
deren <daeaton.chicago@gmail.com>
2016-11-04 15:09:41 -0400

    improved N removal from edges of paired reads with variable lengths

--- ipyrad/assemble/consens_se.py
+++ ipyrad/assemble/consens_se.py
@@ -107,6 +107,7 @@ def hetero(base1, base2):
 
 
 
+
 def removerepeats(consens, arrayed):
     """ 
     Checks for interior Ns in consensus seqs and removes those that are at
@@ -116,22 +117,60 @@ def removerepeats(consens, arrayed):
 
     ## default trim no edges
     consens = "".join(consens).replace("-", "N")
-    edges = [None, None]
 
-    ## trim from left else index starts at zero
-    lcons = len(consens)
-    consens = consens.lstrip("N")
-    edges[0] = lcons - len(consens)
+    ## split for pairs
+    try:
+        cons1, cons2 = consens.split("nnnn")
+        split = consens.index("nnnn")
+        arr1 = arrayed[:, :split]
+        arr2 = arrayed[:, split+4:]
+    except ValueError:
+        cons1 = consens
+        cons2 = ""
+        arr1 = arrayed
+
+    ## trim from left and right of cons1
+    edges = [None, None]
+    lcons = len(cons1)
+    cons1 = cons1.lstrip("N")
+    edges[0] = lcons - len(cons1)
 
     ## trim from right if nonzero
-    lcons = len(consens)
-    consens = consens.rstrip("N")
-    if lcons - len(consens):
-        edges[1] = -1*(lcons - len(consens))
+    lcons = len(cons1)
+    cons1 = cons1.rstrip("N")
+    if lcons - len(cons1):
+        edges[1] = -1*(lcons - len(cons1))
 
     ## trim same from arrayed
-    consens = np.array(list(consens))
-    arrayed = arrayed[:, edges[0]:edges[1]]
+    arr1 = arr1[:, edges[0]:edges[1]]
+
+    ## trim from left and right of cons2 if present
+    if cons2:
+        ## trim from left and right of cons1
+        edges = [None, None]        
+        lcons = len(cons2)
+        cons2 = cons2.lstrip("N")
+        edges[0] = lcons - len(cons2)
+
+        ## trim from right if nonzero
+        lcons = len(cons2)
+        cons2 = cons2.rstrip("N")
+        if lcons - len(cons2):
+            edges[1] = -1*(lcons - len(cons2))
+
+        ## trim same from arrayed
+        arr2 = arr2[:, edges[0]:edges[1]]
+
+        ## reconstitute pairs
+        consens = cons1 + "nnnn" + cons2
+        consens = np.array(list(consens))
+        sep = np.array(arr1.shape[0]*[list("nnnn")])
+        arrayed = np.hstack([arr1, sep, arr2])
+
+    ## if single-end...
+    else:
+        consens = np.array(list(cons1))
+        arrayed = arr1
 
     ## get column counts of Ns and -s
     ndepths = np.sum(arrayed == 'N', axis=0) 
