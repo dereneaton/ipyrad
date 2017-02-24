2fa1b00a25f6eff03c18ce4305190a7bcc3fd36b
deren <daeaton.chicago@gmail.com>
2017-02-24 17:57:54 -0500

    bpp recipe fix

--- conda.recipe/bpp/build.sh
+++ conda.recipe/bpp/build.sh
@@ -3,9 +3,9 @@
 ## go to the src dir
 cd $SRC_DIR/src
 
-gcc -o bpp -03 bpp.c tools.c -lm
-gcc -o bpp-avx -03 -DUSE_AVX -mavx bpp.c tools.c -lm
-gcc -o MCcoal -DSIMULATION bpp.c tools.c -lm 
+gcc -o bpp -O3 bpp.c tools.c -lm
+gcc -o bpp-avx -O3 -DUSE_AVX -mavx bpp.c tools.c -lm
+gcc -o MCcoal -DSIMULATION bpp.c tools.c -lm
 
 cp ./bpp $PREFIX/bin
 cp ./bpp_avx $PREFIX/bin
