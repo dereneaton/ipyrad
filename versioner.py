6440dfd683d95483d4af73493f4a5616fd35f7cb
Isaac Overcast <isaac.overcast@gmail.com>
2017-04-10 12:25:53 -0400

    Prevent versioner from including merge commits in the release notes cuz they are annoying.

--- versioner.py
+++ versioner.py
@@ -51,6 +51,9 @@ for line in fileinput.input(release_file, inplace=1):
         line += "**" + time.strftime("%B %d, %Y") + "**\n\n"
         for commit in commit_lines:
             try:
+                ## Squash merge commits from the releasenotes cuz it annoying
+                if commit[1] == "Merge branch 'master' of https://github.com/dereneaton/ipyrad":
+                    continue
                 line += "- " + commit[1] + "\n"
             except:
                 pass
