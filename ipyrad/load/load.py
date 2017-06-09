b4224cdeef2a64aa86850af7acd227fc957f648c
dereneaton <deren.eaton@yale.edu>
2017-06-09 12:38:44 -0400

    toying with new save/load structure

--- ipyrad/load/load.py
+++ ipyrad/load/load.py
@@ -99,6 +99,19 @@ def update_assembly(data):
 
 
 
+def save_json2(data):
+    """ save to json."""
+
+    ## convert everything to dicts
+    ## skip _ipcluster cuz it's made new.
+    datadict = OrderedDict([
+        ("outfiles", data.__dict__["outfiles"]),
+        ("stats_files", dict(data.__dict__["stats_files"])),
+        ("stats_dfs", data.__dict__["stats_dfs"])
+        ])
+
+
+
 def save_json(data):
     """ Save assembly and samples as json """
 
