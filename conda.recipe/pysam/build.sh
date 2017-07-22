f42635296d06bc054b5467c3432eba6a95763627
Isaac Overcast <isaac.overcast@gmail.com>
2017-07-22 16:02:34 -0700

    Using htslib internal to pysam and removing bcftools/htslib/samtools direct dependencies.

--- conda.recipe/pysam/build.sh
+++ conda.recipe/pysam/build.sh
@@ -10,7 +10,7 @@ export CFLAGS="-I$PREFIX/include"
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
-export HTSLIB_LIBRARY_DIR=$PREFIX/lib
-export HTSLIB_INCLUDE_DIR=$PREFIX/include
+#export HTSLIB_LIBRARY_DIR=$PREFIX/lib
+#export HTSLIB_INCLUDE_DIR=$PREFIX/include
 $PYTHON setup.py install
 
