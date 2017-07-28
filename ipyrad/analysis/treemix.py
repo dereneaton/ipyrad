9be10a6ffe6247e1f2f67d2e6947140623499de0
dereneaton <deren.eaton@yale.edu>
2017-07-28 08:22:27 -0400

    added .draw to treemix object

--- ipyrad/analysis/treemix.py
+++ ipyrad/analysis/treemix.py
@@ -302,6 +302,51 @@ class Treemix(object):
 
 
 
+    def draw(self, axes):
+        """
+        Returns a treemix plot on a toyplot.axes object. 
+        """            
+        ## create a toytree object from the treemix tree result
+        tre = toytree.tree(newick=self.results.tree)
+        tre.draw(
+            axes=axes,
+            use_edge_lengths=True,
+            tree_style='c',
+            tip_labels_align=True,
+            edge_align_style={"stroke-width": 1}
+        );
+
+        ## get coords 
+        for admix in self.results.admixture:
+            ## parse admix event
+            pidx, pdist, cidx, cdist, weight = admix
+            a = _get_admix_point(tre, pidx, pdist)
+            b = _get_admix_point(tre, cidx, cdist)
+
+            ## add line for admixture edge
+            mark = axes.plot(
+                a = (a[0], b[0]),
+                b = (a[1], b[1]),
+                style={"stroke-width": 10*weight,
+                       "stroke-opacity": 0.95, 
+                       "stroke-linecap": "round"}
+            )
+
+            ## add points at admixture sink
+            axes.scatterplot(
+                a = (b[0]),
+                b = (b[1]),
+                size=8,
+                title="weight: {}".format(weight),
+            )
+
+        ## add scale bar for edge lengths
+        axes.y.show=False
+        axes.x.ticks.show=True
+        axes.x.label.text = "Drift parameter"
+        return axes
+
+
     def run(self, ipyclient=None, quiet=True):
 
         ## call command
@@ -387,6 +432,10 @@ class Treemix(object):
 
 
 
+
+
+
+
 def _parse_matrix(mfile, names):
     dat = pd.read_csv(mfile,
                   sep=" ", 
@@ -463,5 +512,30 @@ def _call_treemix(command_list):
         )
     comm = proc.communicate()
     return comm
+
+
+## plotting functions
+
+def _get_admix_point(tre, idx, dist):
+    ## parent coordinates
+    px, py = tre.verts[idx]
+    ## child coordinates
+    cx, cy = tre.verts[tre.tree.search_nodes(idx=idx)[0].up.idx]
+    ## angle of hypotenuse
+    theta = np.arctan((px-cx) / (py-cy))
+    ## new coords along the hypot angle
+    horz = np.sin(theta) * dist
+    vert = np.cos(theta) * dist
+    
+    ## change x
+    a = tre.verts[idx, 0]
+    b = tre.verts[idx, 1] 
+    a -= abs(horz)
+    if py < cy:
+        b += abs(vert)
+    else:
+        b -= abs(vert)
+    return a, b
+
 
 
