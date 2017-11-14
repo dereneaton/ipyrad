8284779eb621938e6e016fb6a1c25f8886000dd4
dereneaton <deren.eaton@yale.edu>
2017-11-14 11:18:31 -0500

    trying alternatives with setup files for jupyter conda bug fix

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ def requires():
         return infile.read().splitlines()
 
 
-def dependency_links():
-    """ return: the package specifications """
-    with open('constraints.txt') as infile:
-        return infile.read().splitlines()
+#def dependency_links():
+#    """ return: the package specifications """
+#    with open('constraints.txt') as infile:
+#        return infile.read().splitlines()
 
 
 ## Auto-update ipyrad version from git repo tag
@@ -36,7 +36,7 @@ setup(
     long_description=open('README.rst').read(),
     packages=find_packages(),
     install_requires=requires(),
-    dependencies=dependency_links(),
+    #dependencies=dependency_links(),
     entry_points={
             'console_scripts': [
                 'ipyrad = ipyrad.__main__:main',
