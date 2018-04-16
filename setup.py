622b04c9674f0350771043bb2f163bf65aee0843
dereneaton <de2356@columbia.edu>
2018-04-15 22:41:49 -0400

    cosmetic

--- setup.py
+++ setup.py
@@ -5,26 +5,19 @@ import glob
 import re
 
 
-
 def requires():
     """ gets packages from requirements.txt """
     with open('requirements.txt') as infile:
         return infile.read().splitlines()
 
 
-#def dependency_links():
-#    """ return: the package specifications """
-#    with open('constraints.txt') as infile:
-#        return infile.read().splitlines()
-
-
-## Auto-update ipyrad version from git repo tag
+# Auto-update ipyrad version from git repo tag
 # Fetch version from git tags, and write to version.py.
 # Also, when git is not available (PyPi package), use stored version.py.
 INITFILE = "ipyrad/__init__.py"
 CUR_VERSION = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                    open(INITFILE, "r").read(),
-                    re.M).group(1)
+                        open(INITFILE, "r").read(),
+                        re.M).group(1)
 
 setup(
     name="ipyrad",
@@ -34,14 +27,14 @@ setup(
     author_email="de2356@columbia.edu",
     description="Interactive assembly and analysis of RADseq data sets",
     long_description=open('README.rst').read(),
-    packages=find_packages(),    
+    packages=find_packages(),
     install_requires=requires(),
-    #dependencies=dependency_links(),
+    # dependencies=dependency_links(),
     entry_points={
-            'console_scripts': [
-                'ipyrad = ipyrad.__main__:main',
-                'tetrad = ipyrad.analysis.__tetrad_cli__:main',
-            ],
+        'console_scripts': [
+            'ipyrad = ipyrad.__main__:main',
+            'tetrad = ipyrad.analysis.__tetrad_cli__:main',
+        ],
     },
     data_files=[('bin', glob.glob("./bin/*"))],
     license='GPL',
