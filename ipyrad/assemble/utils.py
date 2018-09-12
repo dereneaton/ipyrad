36414399e8ab952463191d711590bb45de264447
dereneaton <deren.eaton@yale.edu>
2018-09-12 11:53:53 -0400

    fixed bytes to string in splitalleles for py3

--- ipyrad/assemble/utils.py
+++ ipyrad/assemble/utils.py
@@ -190,23 +190,26 @@ def ambigcutters(seq):
 
 
 def splitalleles(consensus):
-    """ takes diploid consensus alleles with phase data stored as a mixture
-    of upper and lower case characters and splits it into 2 alleles """
+    """ 
+    Takes diploid consensus alleles with phase data stored as a mixture
+    of upper and lower case characters and splits it into 2 alleles 
+    """
 
     ## store two alleles, allele1 will start with bigbase
     allele1 = list(consensus)
     allele2 = list(consensus)
+
     hidx = [i for (i, j) in enumerate(consensus) if j in "RKSWYMrkswym"]
 
     ## do remaining h sites
     for idx in hidx:
-        hsite = consensus[idx]
+        hsite = consensus[idx].encode()
         if hsite.isupper():
-            allele1[idx] = PRIORITY[hsite]
-            allele2[idx] = MINOR[hsite]
+            allele1[idx] = PRIORITY[hsite].decode()
+            allele2[idx] = MINOR[hsite].decode()
         else:
-            allele1[idx] = MINOR[hsite.upper()]
-            allele2[idx] = PRIORITY[hsite.upper()]
+            allele1[idx] = MINOR[hsite.upper()].decode()
+            allele2[idx] = PRIORITY[hsite.upper()].decode()
 
     ## convert back to strings
     allele1 = "".join(allele1)
