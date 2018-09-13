0615a1736f16c13a290cc7d1f78e33b7d3b2746f
dereneaton <deren.eaton@yale.edu>
2018-09-13 13:07:21 -0400

    step7 in assembly uses class

--- ipyrad/core/assembly.py
+++ ipyrad/core/assembly.py
@@ -40,7 +40,7 @@ OUTPUT_FORMATS = {
     'v': 'vcf',
     't': 'treemix',
     'm': 'migrate-n',
-    }  # 'V': 'vcfFull',   ## currently hidden
+    }
     
 
 class Assembly(object):
@@ -1019,47 +1019,6 @@ class Assembly(object):
             else:
                 #assemble.demultiplex.run(self, preview, ipyclient, force)
                 ip.assemble.demultiplex.run2(self, ipyclient, force)
-
-
-    # def _step7func(self, samples, force, ipyclient):
-    #     """ Step 7: Filter and write output files """
-
-    #     ## Get sample objects from list of strings
-    #     samples = _get_samples(self, samples)
-
-    #     if self._headers:
-    #         print("\n  Step 7: Filter and write output files for {} Samples".
-    #               format(len(samples)))
-
-    #     ## Check if all/none of the samples are in the self.database
-    #     try:
-    #         with h5py.File(self.clust_database, 'r') as io5:
-    #             dbset = set([i.decode() for i in io5["seqs"].attrs['samples']])
-    #             iset = set([i.name for i in samples])
-    #             idiff = iset.difference(dbset)
-    #             if idiff:
-    #                 print(NOT_CLUSTERED_YET
-    #                 .format(self.database, ", ".join(list(idiff))))
-
-    #                 ## The the old way that failed unless all samples were
-    #                 ## clustered successfully in step 6. Adding flexibility
-    #                 ## to allow writing output even if some samples failed.
-    #                 ## raise IPyradWarningExit(msg)
-
-    #                 ## Remove the samples that aren't ready for writing out
-    #                 ## i.e. only proceed with the samples that are actually
-    #                 ## present in the db
-    #                 samples = [x for x in samples if x.name not in idiff]
-    #     except (IOError, ValueError):
-    #         raise IPyradError(FIRST_RUN_6.format(self.clust_database))
-
-    #     if not force:
-    #         outdir = os.path.join(self.dirs.project, self.name + "_outfiles")
-    #         if os.path.exists(outdir):
-    #             raise IPyradWarningExit(OUTPUT_EXISTS.format(outdir))
-
-    #     ## Run step7
-    #     ip.assemble.write_outfiles.run(self, samples, force, ipyclient)
 
 
     def _compatible_params_check(self):
