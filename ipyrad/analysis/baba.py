f4ddf62630cda3e874f82de9532983302204af7f
dereneaton <deren.eaton@yale.edu>
2018-10-29 17:29:37 -0400

    comment out baba

--- ipyrad/analysis/baba.py
+++ ipyrad/analysis/baba.py
@@ -21,7 +21,8 @@ import toytree
 ## ipyrad tools
 from ipyrad.analysis.utils import Params, progressbar
 from ipyrad.assemble.utils import IPyradError
-from ipyrad.assemble.write_outfiles import reftrick
+#from ipyrad.assemble.write_outfiles import reftrick    ## TODO REPLACE
+
 from ipyrad.plotting.baba_panel_plot import baba_panel_plot
 
 
