558dc7518c7048396ced5b133bd677a8a31471a5
dereneaton <deren.eaton@yale.edu>
2019-01-04 13:02:09 -0500

    json file loading

--- ipyrad/__main__.py
+++ ipyrad/__main__.py
@@ -54,7 +54,7 @@ class CLI:
         self.parse_params()
 
         # functions below here involve an Assembly object.
-        self._get_assembly()
+        self.get_assembly()
         if self.args.branch:
             self._branch_assembly()
         self._run()
@@ -303,7 +303,8 @@ class CLI:
         for params_file in self.args.merge[1:]:
             self.args.params = params_file
             self.parse_params()  # self.args)
-            assemblies.append(self.get_assembly().data)  # self.args, parsedict)
+            assemblies.append(self.get_assembly().data)  
+            # self.args, parsedict)
 
         ## Do the merge
         merged_assembly = ip.merge(newname, assemblies)
@@ -317,7 +318,7 @@ class CLI:
         sys.exit(1)
 
 
-    def _get_assembly(self):
+    def get_assembly(self):
         """ 
         loads assembly or creates a new one and set its params from 
         parsedict. Does not launch ipcluster. 
@@ -352,37 +353,25 @@ class CLI:
         self.data = data
 
 
-    def _showstats(self):
-        """ loads assembly or dies, and print stats to screen """
+    def show_stats(self):
+        "loads assembly or dies, and print stats to screen"
 
-        project_dir = self.parsedict["project_dir"]
-        if not project_dir:
-            project_dir = "./"
+        # Be nice if the user includes the extension.
+        project_dir = self.parsedict['project_dir']
+        assembly_name = self.parsedict['assembly_name']
+        json_file = os.path.join(project_dir, assembly_name)
+        if not json_file.endswith(".json"):
+            json_file += ".json"
 
-        # Be nice if somebody also puts in the file extension
-        assembly_name = self.parsedict["assembly_name"]
-        my_assembly = os.path.join(project_dir, assembly_name)
-
-        ## If the project_dir doesn't exist don't even bother trying harder.
-        if not os.path.isdir(project_dir):
-            msg = """
-        Trying to print stats for Assembly ({}) that doesn't exist. You must 
-        first run steps before you can show results.
-        """.format(project_dir)
-            sys.exit(msg)
-
-        if not assembly_name:
-            msg = """
-        Assembly name is not set in params.txt, meaning it was either changed or
-        erased since the Assembly was started. Please restore the original name. 
-        You can find the name of your Assembly in the "project dir": {}.
-        """.format(project_dir)
-            raise IPyradError(msg)
+        if not os.path.exists(json_file):
+            raise IPyradWarningExit(
+                "Cannot find assembly {}".format(json_file))
 
         # load the assembly 
-        data = ip.load_json(my_assembly, quiet=True, cli=True)
-        print("\nSummary stats of Assembly {}".format(data.name) \
-             +"\n------------------------------------------------")
+        data = ip.load_json(json_file, quiet=True, cli=True)
+        print(
+            "\nSummary stats of Assembly {}".format(data.name) \
+           +"\n------------------------------------------------")
         
         if not data.stats.empty:
             print(data.stats)
@@ -502,7 +491,7 @@ class CLI:
         
         # show results summary                 
         if self.args.results:
-            self._showstats()
+            self.show_stats()
           
 
 
