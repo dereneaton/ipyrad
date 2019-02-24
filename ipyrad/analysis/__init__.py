98306527abca1d1d690279e2d4360201614f0a20
dereneaton <deren.eaton@yale.edu>
2019-02-24 13:40:17 -0500

    analysis imports going on conda

--- ipyrad/analysis/__init__.py
+++ ipyrad/analysis/__init__.py
@@ -9,19 +9,30 @@ __interactive__ = 1
 ## analysis tools will have a class object that is upper case, which is 
 ## called by a convenience function which is lower case, and has the 
 ## same name as the module (file), such that when we import the function
-## it clobbers the file name as a import. 
+## it clobbers the file name as the import. 
 
-
-#from .tetrad import Tetrad as tetrad
+# conda install raxml -c bioconda
 from .raxml import Raxml as raxml
-from .structure import Structure as structure
-from .tetrad import Tetrad as tetrad
-from .bucky import Bucky as bucky
-
-#from .baba import Baba as baba
-from .bpp import Bpp as bpp
-from .sratools import SRA as sratools
 from .twiist import Twisst as twisst
 from .treeslider import TreeSlider as treeslider
+
+# conda install structure clumpp -c ipyrad
+from .structure import Structure as structure
+
+# conda install tetrad -c eaton-lab
+from .tetrad import Tetrad as tetrad
+
+# conda install bucky -c ipyrad
+# conda install mrbayes -c BioBuilds
+from .bucky import Bucky as bucky
+
+# conda install bpp -c ipyrad
+from .bpp import Bpp as bpp
+
+# conda install sratools -c bioconda
+from .sratools import SRA as sratools
+
+#from .baba import Baba as baba
+
 #from .popgen import Popgen as popgen
-# from .treemix import Treemix as treemix
+#from .treemix import Treemix as treemix
