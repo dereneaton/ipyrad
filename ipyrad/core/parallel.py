5412799d3a4efb7b758861757e15ae05805d2e74
dereneaton <de2356@columbia.edu>
2019-03-04 16:48:55 -0500

    cosmetic

--- ipyrad/core/parallel.py
+++ ipyrad/core/parallel.py
@@ -67,12 +67,13 @@ def start_ipcluster(data):
     ## wrap ipcluster start
     try: 
         # ip.logger.info(shlex.split(standard))
-        subprocess.check_call(shlex.split(standard), 
+        subprocess.check_call(
+            shlex.split(standard), 
             stderr=subprocess.STDOUT,
             stdout=subprocess.PIPE)
 
     except subprocess.CalledProcessError as inst:
-        # ip.logger.debug("  ipcontroller already running.")
+        print(inst)
         raise
 
     except Exception as inst:
