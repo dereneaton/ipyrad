ea3f3a6da8d0f1ad6ef80e6611114f4441282008
dereneaton <deren.eaton@yale.edu>
2019-03-31 21:28:33 -0400

    branching out tetrad

--- ipyrad/analysis/__init__.py
+++ ipyrad/analysis/__init__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-## version is the same as ipyrad
+# version is the same as ipyrad
 from ipyrad import __version__
 
-## set interactive global. Turned off by CLI programs.
-__interactive__ = 1
+# analysis tools will have a class object that is upper case, which is 
+# called by a convenience function which is lower case, and has the 
+# same name as the module (file), such that when we import the function
+# it clobbers the file name as the import. 
 
-## analysis tools will have a class object that is upper case, which is 
-## called by a convenience function which is lower case, and has the 
-## same name as the module (file), such that when we import the function
-## it clobbers the file name as the import. 
+# installed alonside ipyrad: conda install ipyrad -c eaton-lab
+#from tetrad import Tetrad
 
 # conda install raxml -c bioconda
 from .raxml import Raxml as raxml
@@ -20,7 +20,7 @@ from .treeslider import TreeSlider as treeslider
 from .structure import Structure as structure
 
 # conda install tetrad -c eaton-lab
-from .tetrad import Tetrad as tetrad
+#from .tetrad import Tetrad as tetrad
 
 # conda install bucky -c ipyrad
 # conda install mrbayes -c BioBuilds
@@ -32,7 +32,8 @@ from .bpp import Bpp as bpp
 # conda install sratools -c bioconda
 from .sratools import SRA as sratools
 
-#from .baba import Baba as baba
+# no requirements
+from .baba import Baba as baba
 
 #from .popgen import Popgen as popgen
 #from .treemix import Treemix as treemix
