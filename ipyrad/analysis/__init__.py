025d6b46b7c5537867eeaa28e02b9e05cf29d0a2
dereneaton <deren.eaton@yale.edu>
2019-05-30 12:47:12 -0400

    cleaner ipa init

--- ipyrad/analysis/__init__.py
+++ ipyrad/analysis/__init__.py
@@ -8,34 +8,29 @@ from ipyrad import __version__
 # same name as the module (file), such that when we import the function
 # it clobbers the file name as the import. 
 
-# installed alonside ipyrad: conda install ipyrad -c eaton-lab
-#from tetrad import Tetrad
+# conda install mrbayes -c bioconda
+# conda install raxml -c bioconda
+# conda install scikit-learn -c bioconda
+# conda install sratools -c bioconda
+# conda install toytree -c eaton-lab
+# conda install tetrad -c eaton-lab
+# conda install structure -c ipyrad
+# conda install clumpp -c ipyrad
+# conda install bucky -c ipyrad
+# conda install bpp -c ipyrad
 
-# conda install raxml mrbayes -c bioconda
 from .raxml import Raxml as raxml
 from .mrbayes import MrBayes as mrbayes
 from .treeslider import TreeSlider as treeslider
 from .clade_weights import CladeWeights as clade_weights
 # from .twisst import Twisst as twisst
-
-# conda install structure clumpp -c ipyrad
 from .structure import Structure as structure
-from .pca import PCA
-
-# conda install tetrad -c eaton-lab
-#from .tetrad import Tetrad as tetrad
-
-# conda install bucky -c ipyrad
-# conda install mrbayes -c bioconda
+from .pca import PCA as pca
 from .bucky import Bucky as bucky
-
-# conda install bpp -c ipyrad
 from .bpp import Bpp as bpp
 
-# conda install sratools -c bioconda
+#from .tetrad import Tetrad as tetrad
 # from .sratools import SRA as sratools
-
-# no requirements
 # from .baba import Baba as baba
 from .digest_genome import DigestGenome as digest_genome
 #from .popgen import Popgen as popgen
