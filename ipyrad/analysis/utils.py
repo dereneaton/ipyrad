59dd84ac7c2b0526b4b2f0316b0a2f2c0cbf1a1f
dereneaton <deren.eaton@yale.edu>
2019-06-05 16:45:47 -0400

    moved count_snps to utils

--- ipyrad/analysis/utils.py
+++ ipyrad/analysis/utils.py
@@ -4,6 +4,7 @@
 
 # py2/3 compat
 from __future__ import print_function
+from builtins import range
 
 # standard lib
 import datetime
@@ -12,6 +13,7 @@ import sys
 import os
 
 # third party
+import numpy as np
 from numba import njit, prange
 
 
@@ -36,6 +38,52 @@ def get_spans(maparr, spans):
     # drop rows with no span (invariant loci)
     spans = spans[spans[:, 0] != spans[:, 1]]
     return spans
+
+
+
+@njit
+def count_snps(seqarr):
+    nsnps = 0
+    for site in range(seqarr.shape[1]):
+        # make new array
+        catg = np.zeros(4, dtype=np.int16)
+
+        ncol = seqarr[:, site]
+        for idx in range(ncol.shape[0]):
+            if ncol[idx] == 67:    # C
+                catg[0] += 1
+            elif ncol[idx] == 65:  # A
+                catg[1] += 1
+            elif ncol[idx] == 84:  # T
+                catg[2] += 1
+            elif ncol[idx] == 71:  # G
+                catg[3] += 1
+            elif ncol[idx] == 82:  # R
+                catg[1] += 1       # A
+                catg[3] += 1       # G
+            elif ncol[idx] == 75:  # K
+                catg[2] += 1       # T
+                catg[3] += 1       # G
+            elif ncol[idx] == 83:  # S
+                catg[0] += 1       # C
+                catg[3] += 1       # G
+            elif ncol[idx] == 89:  # Y
+                catg[0] += 1       # C
+                catg[2] += 1       # T
+            elif ncol[idx] == 87:  # W
+                catg[1] += 1       # A
+                catg[2] += 1       # T
+            elif ncol[idx] == 77:  # M
+                catg[0] += 1       # C
+                catg[1] += 1       # A
+        # get second most common site
+        catg.sort()
+
+        # if invariant e.g., [0, 0, 0, 9], then nothing (" ")
+        if catg[2] > 1:
+            nsnps += 1
+    return nsnps
+
 
 
 def progressbar(finished, total, start, message):
