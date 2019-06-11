f1cb1d52afef22bf9adcd349473afc479486acaf
dereneaton <deren.eaton@yale.edu>
2019-06-11 15:52:06 -0400

    progress on analysis tools

--- ipyrad/analysis/__init__.py
+++ ipyrad/analysis/__init__.py
@@ -20,26 +20,26 @@ from ipyrad import __version__
 # conda install bucky -c ipyrad
 # conda install bpp -c ipyrad
 
-
 # tested in 0.9.5
 from .raxml import Raxml as raxml
 from .mrbayes import MrBayes as mrbayes
 from .window_extracter import WindowExtracter as window_extracter
 from .treemix import Treemix as treemix
 from .pca import PCA as pca
+from .tetrad import Tetrad as tetrad
 
 # testing
+# from .baba import Baba as baba
+# from .sratools import SRA as sratools
+# from .momi import Momi as momi
+# from .eems import Eems as eems
 from .structure import Structure as structure
 from .treeslider import TreeSlider as treeslider
 from .clade_weights import CladeWeights as clade_weights
+from .digest_genome import DigestGenome as digest_genome
 
 # from .twisst import Twisst as twisst
 from .bucky import Bucky as bucky
 from .bpp import Bpp as bpp
-
-#from .tetrad import Tetrad as tetrad
-# from .sratools import SRA as sratools
-# from .baba import Baba as baba
-from .digest_genome import DigestGenome as digest_genome
 #from .popgen import Popgen as popgen
 
