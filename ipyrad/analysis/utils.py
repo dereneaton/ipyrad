3c8cbaebdf929ec6d675efe719fe6cf46a84d7e6
dereneaton <deren.eaton@yale.edu>
2019-06-15 14:22:26 -0400

    progressbar class

--- ipyrad/analysis/utils.py
+++ ipyrad/analysis/utils.py
@@ -18,6 +18,40 @@ from numba import njit, prange
 
 
 
+class ProgressBar(object):
+    """
+    Print pretty progress bar
+    """       
+    def __init__(self, njobs, start=None, message=""):
+        self.njobs = njobs
+        self.start = (start if start else time.time())
+        self.message = message
+        self.finished = 0
+        
+    @property
+    def progress(self):
+        return 100 * (self.finished / float(self.njobs))
+
+    @property
+    def elapsed(self):
+        return datetime.timedelta(seconds=int(time.time() - self.start))
+ 
+    def update(self):
+        # build the bar
+        hashes = '#' * int(self.progress / 5.)
+        nohash = ' ' * int(20 - len(hashes))
+
+        # print to stderr
+        print("\r[{}] {:>3}% {} | {:<12} ".format(*[
+            hashes + nohash,
+            int(self.progress),
+            self.elapsed,
+            self.message,
+        ]), end="")
+        sys.stdout.flush()
+
+
+
 @njit
 def jsubsample_snps(snpsmap, seed):
     "Subsample snps, one per locus, using snpsmap"
@@ -34,7 +68,7 @@ def jsubsample_snps(snpsmap, seed):
 
 
 
-njit(parallel=True)
+@njit(parallel=True)
 def get_spans(maparr, spans):
     """ 
     Get span distance for each locus in original seqarray. This
