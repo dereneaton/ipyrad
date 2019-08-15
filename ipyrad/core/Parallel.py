b9bdab58b438e627bcbc6a67a887f01bdf7ca2cf
dereneaton <deren.eaton@yale.edu>
2019-08-15 14:16:33 -0400

    raise error if mpi4py is not installed

--- ipyrad/core/Parallel.py
+++ ipyrad/core/Parallel.py
@@ -81,6 +81,17 @@ class Parallel(object):
         # if engines=="MPI" then add --ip arg to view all sockets  
         iparg = ("--ip=*" if "MPI" in self.tool.ipcluster["engines"] else "")
 
+        # check for MPI4PY installation here:
+        try:
+            import mpi4py
+        except ImportError:
+            raise ImportError(
+                "To use MPI you must install an additional library: mpi4py\n" + \
+                "You can do this with the following command: \n" + \
+                "  conda install mpi4py -c conda-forge \n\n" + \
+                "See the ipyrad docs section (Parallelization) for details."
+                )
+
         # make ipcluster arg call
         standard = [
             "ipcluster", "start",
