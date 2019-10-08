e7c7f3bb99bd2044e66eb5fe441dbe11dd81e8c8
Isaac Overcast <isaac.overcast@gmail.com>
2019-10-08 16:57:13 -0400

    Fix bpp Params import

--- ipyrad/analysis/bpp.py
+++ ipyrad/analysis/bpp.py
@@ -16,7 +16,8 @@ from collections import Counter
 
 import numpy as np
 import pandas as pd
-from ipyrad.assemble.utils import IPyradError, Params
+from ipyrad.analysis.utils import progressbar, Params
+from ipyrad.assemble.utils import IPyradError
 
 try:
     import toytree
