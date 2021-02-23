8f45ab90d1cba45184d4660a26444c4936bf35b3
Isaac Overcast <isaac.overcast@gmail.com>
2021-02-23 06:04:00 -0500

    Handle i7 demux to strip trailing newline from barcode

--- ipyrad/assemble/demultiplex.py
+++ ipyrad/assemble/demultiplex.py
@@ -837,7 +837,7 @@ class BarMatch:
 
             # i7 barcodes (get from name string instead of sequence)
             if self.data.hackersonly.demultiplex_on_i7_tags:
-                barcode = read1[0].decode().rsplit(":", 1)[-1].split("+")[0]
+                barcode = read1[0].decode().rsplit(":", 1)[-1].split("+")[0].strip()
 
             else:
                 # COMBINATORIAL BARCODES (BCODE1+BCODE2)
