52ccca1456213b46cd4f3d6031793cea63860193
Deren <de2356@columbia.edu>
2021-09-11 15:02:39 -0400

    new cluster ipyparallel v7

---
+++ ipyrad/core/cluster.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+"""ipyrad Cluster class for starting/stopping ipyparallel.
+
+This uses the new ipyparallel cluster API (v.>7.0) for starting and
+stopping clusters using a context wrapper. Our custom superclass
+of the ipyparallel.Cluster class suppresses some print statements
+of that tool, and adds our own custom logging and exception handling.
+"""
+
+import re
+import os
+import sys
+import time
+import traceback
+import platform
+import subprocess
+from datetime import timedelta
+from loguru import logger
+import IPython
+import ipyparallel
+
+
+logger = logger.bind(name="ipyrad")
+
+
+class Cluster(ipyparallel.cluster.cluster.Cluster):
+    """Custom superclass of ipyparallel cluster.
+
+    Suppresses print statements and adds some logging calls. Exit
+    has custom exception handling and formatting.
+    """
+    # suppress INFO calls from ipyparallel built-in logging.
+    log_level = 30
+
+    def __init__(
+        self, 
+        cores: int, 
+        logger_name: str="ipyrad",
+        **kwargs,
+        ):
+
+        # cores is an alias for .n, logger name adds bind(name=...).
+        self.cores = cores
+        self.n = cores
+        self.logger_name = logger_name
+        super().__init__(**kwargs)
+
+        # hidden attributes
+        self._client_start_time = None
+        self._context_client = None
+
+    def __enter__(self):
+        """Starts a new cluster and connects a client."""
+        logger.bind(name=self.logger_name, end="").info(
+            "Establishing parallel ipcluster: ")
+        self.start_controller_sync()
+        self.start_engines_sync()
+        client = self._context_client = self.connect_client_sync()
+        if self.n:
+            # wait for engine registration
+            client.wait_for_engines(
+                self.n,
+                interactive=False,
+                block=True,
+                timeout=self.engine_timeout,
+            )
+            logger.bind(name=self.logger_name).opt(raw=True).info(
+                f"{len(client)} engines.\n")
+        self._client_start_time = time.time()
+        return client
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        """Ensures shutdown of ipcluster and handling of exceptions."""
+        if self._context_client:
+            self._context_client.close()
+            self._context_client = None
+        self.stop_engines_sync()
+        self.stop_controller_sync()
+        elapsed = int(time.time() - self._client_start_time)
+        elapsed = str(timedelta(seconds=elapsed))        
+        logger.bind(name=self.logger_name).info(
+            f"ipcluster stopped. Elapsed time: {elapsed}")
+
+        # check for and raise any exceptions that occurred.
+        if exc_value:
+
+            # nice message for interrupt.
+            if exc_type == KeyboardInterrupt:
+                logger.error("keyboard interrupt by user, cleaning up.")
+
+            # remote errors raise with IPython ansi colored traceback
+            # which looks like garbage if color is not supported so we
+            # strip this if needed.
+            elif exc_type == ipyparallel.error.RemoteError:
+                trace = "\n".join(exc_value.render_traceback())
+                if not color_support():
+                    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+                    trace = ansi_escape.sub('', trace)
+                logger.error(f"An error occurred on an ipengine:\n{trace}")
+
+            # error not on remote engine, occurred anywhere else.
+            else:
+                trace = traceback.format_exc()
+                logger.error(f"An error occurred:\n{trace}")
+
+
+def get_num_cpus():
+    """Return the effective number of CPUs in the system.
+
+    Returns an integer for either Unix or MacOSX (Darwin). This code
+    is mostly copied from a similar implementation in IPython.
+    If it can't find a sensible answer, it returns 1.
+    """
+    if platform.system() == "Linux":
+        ncpus = os.sysconf("SC_NPROCESSORS_ONLN")
+    else:
+        proc = subprocess.run(
+            ['sysctl', '-n', 'hw.ncpu'], check=True, capture_output=True)
+        ncpus = proc.stdout.decode().strip()
+    try:
+        ncpus = max(1, int(ncpus))
+    except:
+        ncpus = 1
+    return ncpus
+
+
+def color_support():
+    """Check for color support in stderr as a notebook or terminal/tty."""
+    # check if we're in IPython/jupyter
+    tty1 = bool(IPython.get_ipython())
+    # check if we're in a terminal
+    tty2 = sys.stderr.isatty()
+    return tty1 or tty2
