45080d8fdfcb1c43e0a93d3c7ebbcae34672bfda
Deren <de2356@columbia.edu>
2021-09-11 19:00:20 -0400

    log to INFO, need to update many others

--- ipyrad/__init__.py
+++ ipyrad/__init__.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 
-"""
-API level Classes are made available, binary paths are checked,
-and the logger is activated in WARNING mode, but can be modified
-by set_loglevel.
+"""API level classes for ipyrad Assembly.
+
+Examples
+--------
+>>> import ipyrad as ip
+>>> data = ip.Assembly("test")
+>>> data.params.sorted_fastq_path = "..."
+>>> data.run('1', cores=4)
+
+>>> data1 = data.branch("test2", subsamples=['a', 'b'])
+>>> data1.run('2', cores=4)
+>>> data2 = data.branch("test2", subsamples=['a', 'b'])
+>>> data2.run('2', cores=4)
+
+>>> fulldata = data1.merge(data2)
+>>> fulldata.run('3', cores=4)
 """
 
 # bring nested functions to top for API access
@@ -11,9 +23,10 @@ from ipyrad.core.assembly import Assembly
 from ipyrad.core.logger_setup import set_log_level
 from ipyrad.core.load_json import load_json
 from ipyrad.core.merge import merge
+from ipyrad.core.cluster import Cluster
 
 __version__ = "1.0.0-alpha"
 __author__ = "Deren Eaton & Isaac Overcast"
 
 # configure the logger
-set_log_level("WARNING")
+set_log_level("INFO")
