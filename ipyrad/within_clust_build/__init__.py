5a5d881024b287a34cdcd0a6952cae95632af2ed
Deren <de2356@columbia.edu>
2024-07-14 11:42:22 -0400

    added header comments

--- ipyrad/within_clust_build/__init__.py
+++ ipyrad/within_clust_build/__init__.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 
-"""..."""
+"""Step 3 of denovo assembly: build clusters and align.
+
+Substeps:
+1. Cluster are built from (_derep, _matches) files.
+2. Muscle alignment is run on each cluster.
+3. Update Sample objects and save json.
+
+"""
