a3f09203e455e0e7b1c2263a633935be8c1f3ef7
Deren <de2356@columbia.edu>
2024-07-14 11:45:21 -0400

    tests for SE and PE load

--- ipyrad/trim/tests/test_step1.py
+++ ipyrad/trim/tests/test_step1.py
@@ -40,93 +40,135 @@ class TestLoadFastqs(unittest.TestCase):
     def setUp(self):
         # setup dirs
         self.testdir = Path("/tmp/ipyrad-tests")
+        self.testdir.mkdir(exist_ok=True)
         self.tmpdir = self.testdir / "assembly"
         self.datadir = self.testdir / "ipsimdata"
 
-        self.data = ip.Assembly(name="TEST")
-        self.data.params.project_dir = self.tmpdir
+        self.data1 = ip.Assembly(name="TEST-denovo-se")
+        self.data1.params.project_dir = self.tmpdir
+        self.data2 = ip.Assembly(name="TEST-denovo-pe")
+        self.data2.params.project_dir = self.tmpdir
 
-    # def test_load_single_end_fastqs(self):
-    #     """Run basic step1 and check Types of stored results."""
-    #     self.data.params.fastq_paths = "../../../sra-fastqs/4*.fastq"
-    #     self.data.run('1', force=True, cores=6)
+    def test_load_single_end_fastqs(self):
+        """Run basic step1 and check Types of stored results."""
+        self.data1.params.fastq_paths = "../../../sra-fastqs/4*.fastq"
+        self.data1.run('1', force=True, cores=6)
 
-    #     # select a sample
-    #     sample = list(self.data.samples.values())[0]
+        # select a sample
+        self.assertEqual(len(self.data1.samples), 3)
+        sample = list(self.data1.samples.values())[0]
 
-    #     # SE fastqs should be a list with tuples of (Path, '')
-    #     self.assertIsInstance(sample.files.fastqs, list)
-    #     [self.assertIsInstance(i[0], Path) for i in sample.files.fastqs]
-    #     [self.assertEqual(i[1], "") for i in sample.files.fastqs]
+        # SE fastqs should be a list with tuples of (Path, '')
+        self.assertIsInstance(sample.files.fastqs, list)
+        [self.assertIsInstance(i[0], Path) for i in sample.files.fastqs]
+        [self.assertIsInstance(i[1], Path) for i in sample.files.fastqs]
+        [self.assertEqual(i[1].name, "null") for i in sample.files.fastqs]
 
-    #     # SE trimmed should a tuple of (Path, '')
-    #     self.assertIsInstance(sample.files.trimmed, tuple)
-    #     self.assertIsInstance(sample.files.trimmed[0], Path)
-    #     self.assertEqual(sample.files.trimmed[1], "")
+        # SE trimmed should a tuple of (Path, '')
+        self.assertIsInstance(sample.files.trimmed, tuple)
+        self.assertIsInstance(sample.files.trimmed[0], Path)
+        self.assertIsInstance(sample.files.trimmed[1], Path)
+        self.assertEqual(sample.files.trimmed[1].name, "null")
 
-    # def test_load_single_end_fastqs_from_multiple_paths(self):
-    #     """Load samples from more than one location."""
-    #     self.data.params.fastq_paths = [
-    #         "../../../sra-fastqs/40*.fastq",
-    #         "../../../sra-fastqs/2*.fastq",
-    #     ]
-    #     self.data.run('1', force=True, cores=6)
-    #     sample = list(self.data.samples.values())[0]
-    #     self.assertIsInstance(sample.files.fastqs, list)
-    #     self.assertEqual(len(sample.files.fastqs), 1)
-    #     self.assertEqual(len(self.data.samples), 2)
-    #     self.assertEqual(self.data.stats.loc["29154_superba_SRR1754715", "raw_reads"], ...)
+        # reload from JSON and test types again
+        self.data1 = ip.load_json(self.data1.json_file)
+        self.assertIsInstance(sample.files.fastqs, list)
+        [self.assertIsInstance(i[0], Path) for i in sample.files.fastqs]
+        [self.assertIsInstance(i[1], Path) for i in sample.files.fastqs]
+        [self.assertEqual(i[1].name, "null") for i in sample.files.fastqs]
+        self.assertIsInstance(sample.files.trimmed, tuple)
+        self.assertIsInstance(sample.files.trimmed[0], Path)
+        self.assertIsInstance(sample.files.trimmed[1], Path)
+        self.assertEqual(sample.files.trimmed[1].name, "null")
 
-    # def test_load_paired_end_fastqs(self):
-    #     """Load PE data samples one path entered as the only item in a list."""
-    #     self.data.params.fastq_paths = ["../../../pedtest/demux_2023-3-28/linea*.gz"]
-    #     self.data.run('1', force=True, cores=2)
-    #     sample = list(self.data.samples.values())[0]
+    def test_load_single_end_fastqs_from_multiple_paths(self):
+        """Run basic step1 and check Types of stored results."""
+        self.data = self.data1.branch("tmp")
+        self.data.params.fastq_paths = [
+            "../../../sra-fastqs/40*.fastq",
+            "../../../sra-fastqs/2*.fastq",
+        ]
 
-    #     # PE fastqs should be a list with tuples of (Path, '')
-    #     self.assertIsInstance(sample.files.fastqs, list)
-    #     [self.assertIsInstance(i[0], Path) for i in sample.files.fastqs]
-    #     [self.assertIsInstance(i[1], Path) for i in sample.files.fastqs]
+        # run
+        self.data.run('1', force=True, cores=6)
 
-    #     # PE trimmed should a tuple of (Path, '')
-    #     self.assertIsInstance(sample.files.trimmed, tuple)
-    #     self.assertIsInstance(sample.files.trimmed[0], Path)
-    #     self.assertIsInstance(sample.files.trimmed[1], Path)
+        # write to JSON and reload
+        self.data = ip.load_json(self.data.json_file)
 
-    # def test_load_single_end_fastqs_with_techs_merged(self):
-    #     # select two sample's data as inputs
-    #     self.data.params.fastq_paths = [
-    #         "../../../sra-fastqs/2*.fastq",
-    #         "../../../sra-fastqs/40*.fastq",
-    #     ]
-    #     # designate that these two samples are replicates to be merged
-    #     self.data.params.technical_replicates = {
-    #         "TECH": ["29154_superba_SRR1754715", "40578_rex_SRR1754724"],
-    #     }
-    #     # load and trim the reads
-    #     self.data.run('1', force=True, cores=6)
-    #     # fastqs should be [(R1, ''), (R1, '')]
-    #     sample = list(self.data.samples.values())[0]
-    #     self.assertIsInstance(sample.files.fastqs, list)
-    #     [self.assertIsInstance(i[0], Path) for i in sample.files.fastqs]
-    #     [self.assertEqual(i[1], "") for i in sample.files.fastqs]
-    #     self.assertIsInstance(sample.files.trimmed, tuple)
-    #     self.assertIsInstance(sample.files.trimmed[0], Path)
-    #     self.assertEqual(sample.files.trimmed[1], "")
-    #     self.assertEqual(self.data.stats.loc["TECH", "reads_raw"], 2404936)
+        # select a sample
+        self.assertEqual(len(self.data.samples), 2)
+        sample = list(self.data.samples.values())[0]
 
-    # def test_load_single_end_fastqs_with_techs_merged_raise_bad_name(self):
-    #     with self.assertRaises(ValueError):
-    #         self.data.params.fastq_paths = [
-    #             "../../../sra-fastqs/2*.fastq",
-    #             "../../../sra-fastqs/40*.fastq",
-    #         ]
-    #         self.data.params.technical_replicates = {
-    #             "TECH": ["29154_superba_SRR1754715", "XXX"],
-    #         }
-    #         self.data.run('1', force=True, cores=2)
+        # SE fastqs should be a list with tuples of (Path, '')
+        self.assertIsInstance(sample.files.fastqs, list)
+        [self.assertIsInstance(i[0], Path) for i in sample.files.fastqs]
+        [self.assertIsInstance(i[1], Path) for i in sample.files.fastqs]
+        [self.assertEqual(i[1].name, "null") for i in sample.files.fastqs]
 
-    # def test_5_load_paired_end_fastqs_with_techs_not_merged(self):
+        # SE trimmed should a tuple of (Path, '')
+        self.assertIsInstance(sample.files.trimmed, tuple)
+        self.assertIsInstance(sample.files.trimmed[0], Path)
+        self.assertIsInstance(sample.files.trimmed[1], Path)
+        self.assertEqual(sample.files.trimmed[1].name, "null")
+
+    def test_load_single_end_fastqs_with_techs_merged(self):
+        # select two sample's data as inputs
+        self.data = self.data1.branch("tmp")
+        self.data.params.fastq_paths = [
+            "../../../sra-fastqs/2*.fastq",
+            "../../../sra-fastqs/40*.fastq",
+        ]
+        # designate that these two samples are replicates to be merged
+        self.data.params.technical_replicates = {
+            "TECH": ["29154_superba_SRR1754715", "40578_rex_SRR1754724"],
+        }
+        # load and trim the reads
+        self.data.run('1', force=True, cores=6)
+        sample = list(self.data.samples.values())[0]
+        # check fastqs types
+        self.assertIsInstance(sample.files.fastqs, list)
+        [self.assertIsInstance(i[0], Path) for i in sample.files.fastqs]
+        [self.assertIsInstance(i[1], Path) for i in sample.files.fastqs]
+        self.assertEqual(sample.files.fastqs[0][1], Path("null"))
+        # check trimmed types
+        self.assertIsInstance(sample.files.trimmed, tuple)
+        self.assertIsInstance(sample.files.trimmed[0], Path)
+        self.assertIsInstance(sample.files.trimmed[1], Path)
+        self.assertEqual(sample.files.trimmed[1], Path("null"))
+        # expected merged count for TECH sample
+        self.assertEqual(self.data.stats.loc["TECH", "reads_raw"], 2404936)
+
+    def test_load_paired_end_fastqs(self):
+        """Load PE data samples one path entered as the only item in a list."""
+        self.data2.params.fastq_paths = ["../../../pedtest/demux_2023-3-28/linea*.gz"]
+        self.data2.run('1', force=True, cores=2)
+
+        # select a sample
+        self.assertEqual(len(self.data2.samples), 1)
+        sample = list(self.data2.samples.values())[0]
+
+        # PE fastqs should be a list with tuples of (Path, '')
+        self.assertIsInstance(sample.files.fastqs, list)
+        [self.assertIsInstance(i[0], Path) for i in sample.files.fastqs]
+        [self.assertIsInstance(i[1], Path) for i in sample.files.fastqs]
+
+        # PE trimmed should a tuple of (Path, '')
+        self.assertIsInstance(sample.files.trimmed, tuple)
+        self.assertIsInstance(sample.files.trimmed[0], Path)
+        self.assertIsInstance(sample.files.trimmed[1], Path)
+
+        # reload from JSON and test types again
+        self.data2 = ip.load_json(self.data2.json_file)
+        self.assertIsInstance(sample.files.fastqs, list)
+        [self.assertIsInstance(i[0], Path) for i in sample.files.fastqs]
+        [self.assertIsInstance(i[1], Path) for i in sample.files.fastqs]
+        self.assertIsInstance(sample.files.trimmed, tuple)
+        self.assertIsInstance(sample.files.trimmed[0], Path)
+        self.assertIsInstance(sample.files.trimmed[1], Path)
+
+    # def test_load_...
+
+    # def test_load_paired_end_fastqs_with_techs_merged(self):
     #     pass
 
     # def test_user_can_load_dataset_after_fastq_inputs_are_moved(self):
